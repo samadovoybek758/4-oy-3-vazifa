2794e74f9bb61fbdd8835106655e69c0165f9151
samadovoybek758 <oybek22samadov@gmail.com>
2024-06-04 23:17:03 +0500

    ikkinchi

---
+++ js/index.js
@@ -0,0 +1,443 @@
+// 1-masala
+// let a = + prompt("sonni kiriting");
+// let result ="";
+
+// if (a>0){
+//     result ="true";
+// }else {
+//     result ="false";
+// };
+// console.log(result);
+
+
+
+// 2-masala
+// let a = + prompt("sonni kiriting");
+// let result ="";
+
+// if (a%2==1){
+//     result ="true";
+// }else {
+//     result ="false";
+// };
+// console.log(result);
+
+
+
+// 3-masala
+// let a = + prompt("sonni kiriting");
+// let result ="";
+
+// if (a%2==0){
+//     result ="true";
+// }else {
+//     result ="false";
+// };
+// console.log(result);
+
+
+
+// 4-masala
+// let A = + prompt("sonni kiriting");
+// let B = + prompt("sonni kiriting");
+// let result ="";
+
+// if (A>2 && B<=3){
+//     result ="true";
+// }else {
+//     result ="false";
+// };
+// console.log(result);
+
+
+
+// 5-masala
+// let A = + prompt("sonni kiriting");
+// let B = + prompt("sonni kiriting");
+// let result ="";
+
+// if (A>=0 && B<-2){
+//     result ="true";
+// }else {
+//     result ="false";
+// };
+// console.log(result);
+
+
+
+// 6-masala
+// let A = + prompt("A sonni kiriting");
+// let B = + prompt("B sonni kiriting");
+// let C = + prompt("C sonni kiriting");
+// let result ="";
+
+// if (A<=B && B<=C && A<=C){
+//     result ="true";
+// }else {
+//     result ="false";
+// };
+// console.log(result);
+
+
+
+// 7-masala
+// let A = + prompt("A sonni kiriting");
+// let B = + prompt("B sonni kiriting");
+// let C = + prompt("C sonni kiriting");
+// let result ="";
+
+// if (A>B && B>C || A<B && B<C){
+//     result ="true";
+// }else {
+//     result ="false";
+// };
+// console.log(result);
+
+
+
+// 8-masala
+// let A = + prompt("sonni kiriting");
+// let B = + prompt("sonni kiriting");
+// let result ="";
+
+// if (A%2==1 && B%2==1){
+//     result ="true";
+// }else {
+//     result ="false";
+// };
+// console.log(result);
+
+
+
+// // 10-masala
+// let A = + prompt("sonni kiriting");
+// let B = + prompt("sonni kiriting");
+// let result ="";
+
+// if (A%2==1 && B%2==0 || A%2==0 && B%2==1){
+//     result ="true";
+// }else {
+//     result ="false";
+// };
+// console.log(result);
+
+
+
+// 11-masala
+// let A = + prompt("sonni kiriting");
+// let B = + prompt("sonni kiriting");
+// let result ="";
+
+// if (A%2==0 && B%2==0 || A%2==1 && B%2==1){
+//     result ="true";
+// }else {
+//     result ="false";
+// };
+// console.log(result);
+
+
+
+// // 12-masala
+// let A = + prompt("A sonni kiriting");
+// let B = + prompt("B sonni kiriting");
+// let C = + prompt("C sonni kiriting");
+// let result ="";
+
+// if (A>0 && B>0 && C>0){
+//     result ="true";
+// }else {
+//     result ="false";
+// };
+// console.log(result);
+
+
+
+// 13-masala
+// let A = + prompt("A sonni kiriting");
+// let B = + prompt("B sonni kiriting");
+// let C = + prompt("C sonni kiriting");
+// let result ="";
+
+// if (A>0){
+//     result ="true";
+// }else if(B>0) {
+//     result ="true";
+// }else if (C>0){
+//     result="true"
+// }else{
+//     result="false"
+// }
+// console.log(result);
+
+
+
+// 14-masala
+// let A = + prompt("A sonni kiriting");
+// let B = + prompt("B sonni kiriting");
+// let C = + prompt("C sonni kiriting");
+// let result ="";
+
+// if (A>0 && B<0 && C<0 ){
+//     result ="true";
+// }else if(B>0 && A<0 && C<0) {
+//     result ="true";
+// }else if (C>0 && B>0 && A<0){
+//     result="true"
+// }else{
+//     result="false"
+// }
+// console.log(result);
+
+// 15-masala!
+// let A = + prompt("A sonni kiriting");
+// let B = + prompt("B sonni kiriting");
+// let C = + prompt("C sonni kiriting");
+// let result ="";
+
+// if (A>0 && B>0 && C<0 ){
+//     result ="true";
+// }else if(B>0 && A<0 && C>0) {
+//     result ="true";
+// }else if (C>0 && B>0 && A>0){
+//     result="true"
+// }else{
+//     result="false"
+// }
+// console.log(result);
+
+
+
+// 16-masala
+// let a = + prompt("sonni kiriting")
+// let result = ""
+
+// if(a>9 && a<100 && a%2==0){
+//     result = "true"
+// }else{
+//     result = "false"
+// }
+// console.log(result);
+
+
+
+// // 17-masala
+// let a = + prompt("sonni kiriting")
+// let result = ""
+
+// if(a%2==0){
+//     result = ++a
+// }else{
+//     result = a
+// }
+// console.log(result);
+
+
+
+// // 18-masala
+// let a = + prompt("sonni kiriting")
+// let result = ""
+
+// if(a%2==0){
+//     result = ++a
+// }else{
+//     result = a-2
+// }
+// console.log(result);
+
+
+
+// 19-masala
+// let a = + prompt("sonni kiriting")
+// let result = ""
+
+// if(a>0){
+//     result = ++a
+// }else if (a==0){
+//     result ="10"
+// }else{
+//     result = a-2
+// }
+// console.log(result);
+
+
+
+
+// 20-masala !
+// let a = + prompt("birinchi sonni kiriting")
+// let b = + prompt("ikkinchi sonni kiriting")
+// let c = + prompt("uchinchi sonni kiriting")
+// let result = ""
+
+// if(a>0 && b>0 && c>0){
+//     result ="3 ta"
+// } else if (a<0 && b>0 && c>0   && a>0 && b<0 && c>0  && a>0 && b>0 && c<0){
+//     result ="2 ta"
+// }else{
+//     result = a-2
+// }
+// console.log(result);
+
+
+// 21-masala
+
+
+
+
+// 22-masala
+// let a = + prompt("birinchi sonni kiriting")
+// let b = + prompt("ikkinchi sonni kiriting")
+// let result = ''
+
+// if (a>b){
+//     result = a
+// }else{
+//     result =b
+// }
+// console.log(result);
+
+
+
+// // 23-masala
+// let a = + prompt("birinchi sonni kiriting");
+// let b = + prompt("ikkinchi sonni kiriting");
+// let result = '';
+
+// if (a>b){
+//     result = b
+// }else{
+//     result = a
+// };
+// console.log(result);
+
+
+
+// 24-masala
+// let a = + prompt("birinchi sonni kiriting");
+// let b = + prompt("ikkinchi sonni kiriting");
+// let result = '';
+// let result_2 = ''
+
+// if (a>b){
+//     result = a;
+//     result_2 = b;
+// }else{
+//     result = b;
+//     result_2 = a;
+// };
+// console.log(result);
+// console.log(result_2);
+
+
+
+// 25-masala
+// let a = + prompt("a sonni kiriting");
+// let b = + prompt("b sonni kiriting");
+// let result = '';
+// let result_2 = ''
+
+// if (a>b){
+//     result = a;
+//     result_2 = b+a;
+// }else{
+//     result = a;
+//     result_2 = b;
+// };
+// console.log(result);
+// console.log(result_2);
+
+
+
+// 26-masala
+// let a = + prompt("birinchi sonni kiriting");
+// let b = + prompt("ikkinchi sonni kiriting");
+// let result = '';
+// let result_2 = '';
+
+// if (a==b){
+//     result = 0;
+//     result_2 = 0;
+// }else{
+//     result = a+b;
+//     result_2 = a+b;
+// };
+// console.log(result);
+// console.log(result_2);
+
+
+
+// 27-masala !
+// let a = + prompt("birinchi sonni kiriting");
+// let b = + prompt("ikkinchi sonni kiriting");
+// let result = '';
+// let result_2 = '';
+
+// if (a>b){
+
+// }
+// if (a==b){
+//     result = 0;
+//     result_2 = 0;
+// }else{
+//     result = a+b;
+//     result_2 = a+b;
+// };
+// console.log(result);
+// console.log(result_2);
+
+
+
+// // 28-masala
+// let a = + prompt("birinchi sonni kiriting");
+// let b = + prompt("ikkinchi sonni kiriting");
+// let c = + prompt("uchinchi sonni kiriting");
+
+// let result =""
+
+// if (a>b && b>c && a>c){
+//     result =c
+// }else if(a>b && b<c && a>c){
+//     result =b
+// }else{
+//     result=a
+
+// }
+// console.log(result);
+
+
+
+// 29-masala
+// let a = + prompt("birinchi sonni kiriting");
+// let b = + prompt("ikkinchi sonni kiriting");
+// let c = + prompt("uchinchi sonni kiriting");
+
+// let result =""
+
+// if (a>b>c || a<b<c){
+//     result =b
+// }else if(a>c>b || a<c<b){
+//     result =c
+
+// } else {
+//      result=a
+
+// }
+// console.log(result);
+
+
+
+// 30-masala
+// let a = + prompt("birinchi sonni kiriting");
+// let b = + prompt("ikkinchi sonni kiriting");
+// let c = + prompt("uchinchi sonni kiriting");
+
+// let result =""
+
+// if (a>b && b>c && a>c){
+//     result =c
+// }else if(a>b && b<c && a>c){
+//     result =b
+// }else{
+//     result=a
+
+// }
+// console.log(result);
